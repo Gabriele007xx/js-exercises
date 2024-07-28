a13fd6c17f76a2ebe87c4a5eec2a1262b5d63233
Gabriele Lo Re <gabrolr70@gmail.com>
2024-07-28 17:21:53 +0200

    Update tire.js

--- automobile/tire.js
+++ automobile/tire.js
@@ -1,15 +1,26 @@
 const tireCanvas = document.getElementById('tireCanvas');
 const ctx2 = tireCanvas.getContext('2d');
 
-function drawTire(centerX, centerY, radius)
+function drawTire(centerX, centerY, totalRadius, rimRadius)
 {
   // Begin a new path for the circle
   ctx2.beginPath();
-  // Draw the circle
+  // tire
   ctx2.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
   // Set the fill color to black
   ctx2.fillStyle = 'black';
   // Fill the circle with the black color
   ctx2.fill();
+  
+  // Draw the rim
+  ctx2.arc(centerX, centerY, InchToMM(rimRadius), 0, 2 * Math.PI, false);
+  // Set the fill color to black
+  ctx2.fillStyle = 'gray';
+  // Fill the circle with the black color
+  ctx2.fill();  
 }
 
+function InchToMM(inches)
+{
+  return (inches*2.54) * 10;
+}
